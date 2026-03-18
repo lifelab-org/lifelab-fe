d4cc928158f4b4f2cd3b5678ebf85ab2ba9c6e7d
영 <essp0521@gmail.com>
2026-03-18 23:12:26 +0900

    Revert "Feat/일일기록"

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
-import DetailLayout from "./layouts/DetailLayout";
+import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Calendar from "./pages/Calendar";
 import Archive from "./pages/Archive";
-import DailyRecord from "./pages/DailyRecord";
-import RecordCondition from "./pages/RecordCondition";
+import BottomNav from "./components/BottomNav";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/archive" element={<Archive />} />
-        </Route>
+      <div className="max-w-md mx-auto min-h-[100dvh] bg-white relative shadow-xl overflow-hidden flex flex-col font-sans">
         
-        <Route element={<DetailLayout />}>
-          <Route path="/record" element={<DailyRecord />} />
-          <Route path="/record/condition" element={<RecordCondition />}></Route>
-        </Route>
-      </Routes>
+        <main className="flex-1 overflow-y-auto pb-[80px]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/archive" element={<Archive />} />
+          </Routes>
+        </main>
+        
+        {/* 하단 네비게이션 바 */}
+        <BottomNav />
+      </div>
     </BrowserRouter>
   );
 }
