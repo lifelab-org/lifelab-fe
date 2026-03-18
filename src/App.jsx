164bda2d6e4492bf7917d217ec2d25acf515cd05
Goeunwoo <avouchre24@gmail.com>
2026-03-18 22:59:47 +0900

    feat: 온보딩 화면 UI 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,39 @@
-import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Calendar from "./pages/Calendar";
 import Archive from "./pages/Archive";
 import BottomNav from "./components/BottomNav";
-
+import Onboarding from "./pages/onboarding/Onboarding";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="max-w-md mx-auto min-h-[100dvh] bg-white relative shadow-xl overflow-hidden flex flex-col font-sans">
-        
-        <main className="flex-1 overflow-y-auto pb-[80px]">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="/archive" element={<Archive />} />
-          </Routes>
-        </main>
-        
-        {/* 하단 네비게이션 바 */}
-        <BottomNav />
-      </div>
+      <Routes>
+        <Route path="/onboarding" element={<Onboarding />} />
+
+        <Route
+          path="/*"
+          element={
+            <div className="max-w-md mx-auto min-h-[100dvh] bg-white relative shadow-xl overflow-hidden flex flex-col font-sans">
+              <main className="flex-1 overflow-y-auto pb-[80px]">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/calendar" element={<Calendar />} />
+                  <Route path="/archive" element={<Archive />} />
+                </Routes>
+              </main>
+
+              <BottomNav />
+            </div>
+          }
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
