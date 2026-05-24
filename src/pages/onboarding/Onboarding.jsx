f7af2898297457d1b3bc6eff5dd170d0fef63474
Goeunwoo <avouchre24@gmail.com>
2026-05-24 12:38:22 +0900

    api 연결 수정 중

--- src/pages/onboarding/Onboarding.jsx
+++ src/pages/onboarding/Onboarding.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 import "./Onboarding.css";
 
 import logo from "../../assets/lifelab_logo.png";
@@ -7,23 +6,8 @@ import char from "../../assets/lifelab_char.png";
 import talk from "../../assets/talk.png";
 
 function Onboarding() {
-  const handleKakaoLogin = async () => {
-    try {
-      // 1. 백엔드 명세서 스펙대로 카카오 로그인 인가 URL을 요청합니다 (쿠키 사용 X)
-      const res = await axios.get("https://life-lab.shop/api/auth/kakao/login");
-
-      console.log("카카오 URL 응답:", res.data);
-
-      // 2. 응답받은 데이터 안에 redirectUrl이 있다면 그 주소창으로 브라우저를 이동시킵니다.
-      if (res.data && res.data.data && res.data.data.redirectUrl) {
-        window.location.href = res.data.data.redirectUrl;
-      } else {
-        console.error("응답 데이터 구조가 명세서와 다릅니다:", res.data);
-      }
-    } catch (err) {
-      console.error("카카오 로그인 인가 URL을 가져오는 중 에러 발생:", err);
-      alert("로그인 페이지를 불러오지 못했습니다. 서버 상태를 확인해 주세요.");
-    }
+  const handleKakaoLogin = () => {
+    window.location.href = "https://life-lab.shop/api/auth/kakao/login";
   };
 
   return (
